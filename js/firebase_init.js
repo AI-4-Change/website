490782ea33c32814e1d4ee9763c81fe4a989e25e
Akshat Adsule <akshat.adsule@gmail.com>
2021-01-13 14:52:01 -0800

    load blogs from firebase

--- js/firebase_init.js
+++ js/firebase_init.js
@@ -1,18 +1,5 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebase_core = document.createElement("script")
-firebase_core.setAttribute('src','https://www.gstatic.com/firebasejs/8.2.2/firebase-app.js');
-document.body.appendChild(firebase_core);
-
-var firebase_analytics = document.createElement("script")
-firebase_analytics.setAttribute('src','https://www.gstatic.com/firebasejs/8.2.2/firebase-analytics.js');
-document.body.appendChild(firebase_analytics);
-
-var firebase_core = document.createElement("script")
-firebase_core.setAttribute('src','https://www.gstatic.com/firebasejs/8.2.2/firebase-app.js');
-document.body.appendChild(firebase_core);
-
-
 var firebaseConfig = {
   apiKey: "AIzaSyAYUXBdJhV0owXqi0qEa1hQtOUiPSdARBE",
   authDomain: "ai4change-hhs.firebaseapp.com",
---
+++ js/load_blogs.js
@@ -0,0 +1,49 @@
+let db = firebase.firestore();
+const articles_section = document.getElementById("articles");
+
+db.collection("blogs")
+  .orderBy("timestamp", "desc")
+  .get()
+  .then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      let data = doc.data();
+      let article = document.createElement("div");
+      article.className = "article";
+
+      let post_title = document.createElement("h2");
+      post_title.innerText = data.title;
+      article.appendChild(post_title);
+
+      // post meta data
+      let post_meta = document.createElement("div");
+      post_meta.className = "meta";
+
+      let meta_img = document.createElement("img");
+      meta_img.setAttribute(
+        "src",
+        `assets/team/${data.author.split(" ")[0].toLowerCase()}.png`
+      );
+      meta_img.className = "meta-author-img";
+
+      let meta_author = document.createElement("p");
+      meta_author.innerText = data.author;
+
+      post_meta.appendChild(meta_img);
+      post_meta.appendChild(meta_author);
+      article.appendChild(post_meta);
+
+      // desc
+      let post_desc = document.createElement("p");
+      post_desc.innerHTML = data.desc;
+      article.appendChild(post_desc);
+
+      // link
+      let post_link = document.createElement("a");
+      post_link.classList = ["btn btn-outline-dark"];
+      post_link.setAttribute("href", data.link);
+      post_link.innerHTML = `Read Article <i class="fas fa-external-link-alt"></i>`;
+      article.appendChild(post_link);
+
+      articles_section.appendChild(article);
+    });
+  });
